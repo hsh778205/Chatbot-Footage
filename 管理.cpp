d27f2bf82378888a971b6948fe849113c8ca0123
何劭弘 <51521470+hsh778205@users.noreply.github.com>
2020-04-02 22:55:47 +0800

    构造基本的框架

---
+++ 管理.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<algorithm>
+#include<cstdio>
+#include<cstring>
+#include<cmath>
+#include<map>
+#include<set>
+#include<queue>
+#include<vector>
+#define IL inline
+#define re register
+#define LL long long
+using namespace std;
+
+struct sen{
+	string a,b;
+};
+
+vector<sen>lib;
+
+int main()
+{
+	
+	
+	return 0;
+}
+
